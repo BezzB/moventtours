7168caa4a32770610a29f5e7a22eae3625918257
Bezalell Barnet W <bezbanz@gmail.com>
2024-02-23 14:32:33 +0300

    Update code

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+  { href: '/', key: 'how_hilink_work', label: 'How MoreVent Works?' },
   { href: '/', key: 'services', label: 'Services' },
   { href: '/', key: 'pricing ', label: 'Pricing ' },
   { href: '/', key: 'contact_us', label: 'Contact Us' },
@@ -52,7 +52,7 @@ export const FOOTER_LINKS = [
   {
     title: 'Learn More',
     links: [
-      'About Hilink',
+      'About MoreVent',
       'Press Releases',
       'Environment',
       'Jobs',
@@ -62,15 +62,15 @@ export const FOOTER_LINKS = [
   },
   {
     title: 'Our Community',
-    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    links: ['Climbing mount-kenya', 'Hiking longonot', 'Ngong Hiking trail'],
   },
 ];
 
 export const FOOTER_CONTACT_INFO = {
   title: 'Contact Us',
   links: [
-    { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: 'Admin Officer', value: '0701542227' },
+    { label: 'Email Officer', value: 'morevent@gmail.com' },
   ],
 };
 
